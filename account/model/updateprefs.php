b39b32550cd851674658f987edde98d8015f2191
averonski <averonski@gmail.com>
2014-06-16 11:20:09 -0400

    Update updateprefs.php
    added comments

--- account/model/updateprefs.php
+++ account/model/updateprefs.php
@@ -1,12 +1,17 @@
 <?php
-
+	//account/model/updateprefs.php
+	//updates a persons preferences
+	
+	//initailizes person class
 	$person = new Person();
-
+	
+	//sets preferences
 	$person->setPerson_id($_GET['pid']);
 	$person->setPrefEmail($_GET['email']);
 	$person->setPrefMail($_GET['mail']);
 	$person->setPrefPhone($_GET['calls']);
-
+	
+	//updates database
 	$update = $dbio->updatePrefs($person);
 
 ?>
