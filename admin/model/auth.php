bc822e0ff322ab70774261d21126f8eb47bd0e44
averonski <averonski@gmail.com>
2014-06-16 12:10:50 -0400

    Update auth.php
    added comments

--- admin/model/auth.php
+++ admin/model/auth.php
@@ -1,28 +1,27 @@
 <?php
 
-	// TITLE: Admin Authrorization Model
-	// FILE: admin/model/auth.php
+	// admin/model/auth.php
 	// AUTHOR: sbkedia
-
-
+	
+	//gets all pending hours for work authorizations 
 	function readHours(){
 		global $dbio;
 		$workAuthorization = $dbio->readPendingWorkAuthorization();
 		return $workAuthorization;
 	}
-
+	//gets person info from person id
 	function readPerson($personId){
 		global $dbio;
 		$personDetails = $dbio->readPerson($personId);
 		return $personDetails;
 	}
-
+	//gets event info from event id
 	function readEvent($eventId){
 		global $dbio;
 		$eventDetails = $dbio->readEvent($eventId);
 		return $eventDetails;
 	}
-
+	//gets donation info for donation authorizations
 	function readDonations(){
 		global $dbio;
 		$donationInfo = $dbio->readPendingDonationAuthorizations();
