28da1eb5f1401aab524143a23a12b726e7d15d38
averonski <averonski@gmail.com>
2014-06-26 12:25:07 -0400

    Update checkEmail.php

--- login/view/checkEmail.php
+++ login/view/checkEmail.php
@@ -1,4 +1,9 @@
 <?php
+
+// TITLE: account personal info view
+// FILE: login/view/checkEmail.php
+// AUTHOR: 
+
 global $act;
 global $msg;
 
@@ -6,6 +11,7 @@ global $msg;
 
 <html>
 <body>
+<!--submits email to server-->
 <form>
 	<input name="act" type="hidden" value="<?php echo $act; ?>" >
 	Email id: <input type="text" name="email">
